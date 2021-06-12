bc01d40d9b24856420c553a6d5362e117f1e37e0
Yaniv (Jan) Shalom <85073419+JanShalom@users.noreply.github.com>
2021-06-12 14:36:02 +0300

    Update invoke-copy-blob-to-vm.ps1

--- Mission6/invoke-copy-blob-to-vm.ps1
+++ Mission6/invoke-copy-blob-to-vm.ps1
@@ -1,3 +1,7 @@
 #run from cloud shell
-Invoke-AzVMRunCommand -ResourceGroupName ms-jan  -Name Jan-VM1 -CommandId 'RunPowerShellScript' -ScriptPath ./copy-blob-to-vm.ps1
+#Invoke-AzVMRunCommand -ResourceGroupName ms-jan  -Name Jan-VM1 -CommandId 'RunPowerShellScript' -ScriptPath ./copy-blob-to-vm.ps1
+
+#Run from Local PC
+Invoke-AzVMRunCommand -ResourceGroupName ms-jan  -Name Jan-VM1 -CommandId 'RunPowerShellScript' -ScriptPath .\Mission6\invoke-copy-blob-to-vm.ps1
+
 
